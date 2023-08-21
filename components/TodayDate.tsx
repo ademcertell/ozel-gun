f1ae5ad6ec196cba77f592e59e4cbcc6d699a1d9
ademcertell <ademcancertel619@gmail.com>
2023-08-22 00:24:19 +0300

    reponsive design

--- components/TodayDate.tsx
+++ components/TodayDate.tsx
@@ -28,22 +28,22 @@ const TodayDate: React.FC<TodayDateProps> = ({ className }) => {
   const formattedDate = `${day} ${months[monthIndex]} ${year}`;
 
   return (
-    <div className="flex items-center space-x-1">
+    <div className={`flex items-center space-x-2 ${className}`}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
         stroke="currentColor"
-        className="w-6 h-6 text-blue-500"
+        className="w-4 h-4 text-blue-500"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={1.5}
           d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
         />
       </svg>
-      <p className={className}>{formattedDate}</p>
+      <p className="text-sm md:text-base">{formattedDate}</p>
     </div>
   );
 };
